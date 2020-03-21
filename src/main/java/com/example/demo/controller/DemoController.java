ef983c1cb437b8d4c9b6349d1a3936fbbb8e6c31
NhungPham98 <phamthicamnhung510@gmail.com>
2020-03-21 11:33:51 +0700

    v3

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,18 +1,34 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.model.User;
 
 @Controller
 public class DemoController {
-	public DemoController()
-	{
+	public DemoController() {
 		System.out.print("dddddđsssssssssssssss");
 	}
-	
+
 	@RequestMapping("/")
 	public String index() {
 		return "index.html";
+	}
+
+	@RequestMapping("/login")
+	public String Login() {
+		return "login";
+	}
+
+	@PostMapping("/submitlogin")
+	public ModelAndView SubmitLogin(@ModelAttribute("user") User user) {
+		ModelAndView model = new ModelAndView("userEdit");
+		model.addObject("user", user);
+		return model;
 	}
 
 }
